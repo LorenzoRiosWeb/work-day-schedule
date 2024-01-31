43cfda847a2f31a641b3730ffa93143425949f66
LorenzoRiosWeb <100882991+LorenzoRiosWeb@users.noreply.github.com>
2024-01-30 20:25:40 -0700

    changed from moment js to day js as project requires day js

--- assets/script.js
+++ assets/script.js
@@ -9,22 +9,40 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
+    $(document).ready(function(){
+      // add saveBtn event listener
+      $(".saveBtn").on('click', function(){
+        var text = $(this).sibling(".description").val();
+        var time = $(this).parent().attr("id");
+        // save in local storage
+        localStorage.setItem(time,text);
+      })
+    })
+
+
+
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
     //
+
+
+
+
+
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
+
+
+
+
+
     // TODO: Add code to display the current date in the header of the page.
-    function displayCurrentDay() {
-      var currentDate = moment().format('dddd MMMM do YYYY');
-      $('#currentDay').text(currentDate);
-    }
-    displayCurrentDay();
+    $("#currentDay").text(dayjs().format("dddd, MMMM DD"));
+
   });
 
   
